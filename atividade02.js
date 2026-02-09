a234cc6d7209005304163df41c13cc60bd1c02b9
Isabella <isabella.cabrera@aluno.senai.br>
2026-02-09 09:46:10 -0300

    Feat: Atualiza atividade 02

--- atividade02.js
+++ atividade02.js
@@ -1,14 +1,19 @@
 // 6. Multiplicar por 10
 const multiDez = n => n * 10;
+console.log(multiDez(2));
 
 // 7. Calcular idade em Dias
 const idadeEmDias = anos => anos * 365;
+console.log(idadeEmDias(17));
 
 // 8. Converter para Maiúsculo
 const gritar = texto => texto.toUpperCase();
+console.log(gritar("socorro"))
 
 // 9. Desconto de 5%
 const aplicarDesconto = valor => valor * 0.05;
+console.log(aplicarDesconto(100))
 
 // 10.Subtração Simples
 const subtrair = (a, b) => a - b;
+console.log(subtrair(4, 2))
