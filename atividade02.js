f731773cb1f6fc805c22ddf0793cfb54c615bf58
Isabella <isabella.cabrera@aluno.senai.br>
2026-02-09 09:25:03 -0300

    Feat: Adiciona atividade 02

---
+++ atividade02.js
@@ -0,0 +1,14 @@
+// 6. Multiplicar por 10
+const multiDez = n => n * 10;
+
+// 7. Calcular idade em Dias
+const idadeEmDias = anos => anos * 365;
+
+// 8. Converter para Maiúsculo
+const gritar = texto => texto.toUpperCase();
+
+// 9. Desconto de 5%
+const aplicarDesconto = valor => valor * 0.05;
+
+// 10.Subtração Simples
+const subtrair = (a, b) => a - b;
